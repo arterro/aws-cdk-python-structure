c559fade9d1d9ab858f9681c261ae2bf9446fe70
arterro <github@arter.ro>
2022-01-20 19:47:14 -0800

    Begin templating

---
+++ app.py
@@ -0,0 +1,7 @@
+from aws_cdk import core as cdk
+
+import constants
+
+app = cdk.App()
+
+app.synth()
---
+++ constants.py
@@ -0,0 +1,6 @@
+import os
+
+from aws_cdk import core as cdk
+
+CDK_APP_NAME = "SbxApp"
+CDK_APP_PYTHON_VERSION = "3.7"
